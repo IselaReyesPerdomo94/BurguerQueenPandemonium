fdf7a79c9c0e55b7fb4eb5eaae8ad2c4fab713f8
LedaHuerta <ledamhuerta@gmail.com>
2019-09-14 23:20:16 -0500

    subiendo componente de tittles y vista de settings

--- my-app/src/components/titles/index.jsx
+++ my-app/src/components/titles/index.jsx
@@ -1,2 +1,15 @@
 import React from 'react';
 import './style.css';
+import TittleText from './text/text'
+import TittleIcon from './icon/tittleIcon'
+
+const Tittle = ({color, text, icon}) => {
+    return (
+        <header style={{backgroundColor:color}}className="header">
+            <TittleIcon iconLink={icon} className="icon"/>
+            <TittleText text={text}/>
+        </header>
+    )
+}
+
+export default Tittle;
---
+++ my-app/src/views/settings.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Tittle from '../components/titles/index';
+
+const Settings = () => {
+    return (
+        <Tittle color = "#303F9F"  text = "Configuración" icon = {<i className="material-icons icon">settings_applications</i>}/>
+    )
+}
+
+export default Settings;
