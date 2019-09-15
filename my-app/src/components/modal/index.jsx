d90585baec97c68ce19b29299fed05b9a634f06a
carolinaab <tau_karo@hotmail.com>
2019-09-15 14:45:47 -0500

    se agregan estilos al modal

--- my-app/src/components/modal/index.jsx
+++ my-app/src/components/modal/index.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import "./style.css"
+import "./style.css";
+import { Link } from "react-router-dom";
+import { Input } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-function Modal({open, close}) {
-  const classes =  `overlay ${open ? "open": "closed"}`
+function Modal({ open, close }) {
+  const classes = `overlay ${open ? "open" : "closed"}`
   return (
     <div className={classes}>
-      <div className="modal">YA QUEDO EL MODAL</div>
-      <input type="button" className="close-modal-button" onClick={close}/>
+      <div className="modal">
+        <div className="modal-title">
+          <Link to="/">
+            <input type="button" className="close-modal-button" value="X" onClick={close} />
+          </Link>
+
+          <h3>Introduce tu PIN</h3>
+        </div>
+        <div className="modal-input">
+          <TextField id="outlined-password-input" label="Password" className={classes.textField} type="password" autoComplete="current-password" margin="normal" variant="outlined" />
+          <Button variant="contained" className={classes.button} className="button-modal" onClick={close}>Entrar</Button>
+        </div>
+      </div>
     </div>
   )
 }
