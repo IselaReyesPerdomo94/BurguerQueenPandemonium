36d0914d9d35ff6b32ed063e345d54934a01c126
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-09-16 15:55:59 -0500

    Updates

---
+++ precache-manifest.137d65671aea1e357d5cb766c5059bee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "002c5120bca22f44cbf71c20d09920e2",
+    "url": "/BurguerQueenPandemonium/index.html"
+  },
+  {
+    "revision": "65fb0053e20978daf4df",
+    "url": "/BurguerQueenPandemonium/static/css/main.8b321ad2.chunk.css"
+  },
+  {
+    "revision": "75e26998e19c32a41151",
+    "url": "/BurguerQueenPandemonium/static/js/2.1500520a.chunk.js"
+  },
+  {
+    "revision": "65fb0053e20978daf4df",
+    "url": "/BurguerQueenPandemonium/static/js/main.3e69c0f3.chunk.js"
+  },
+  {
+    "revision": "fa4453b66e8dddac19b6",
+    "url": "/BurguerQueenPandemonium/static/js/runtime~main.28881ce3.js"
+  },
+  {
+    "revision": "399501f0dc1119d29363c264bb3444e0",
+    "url": "/BurguerQueenPandemonium/static/media/pandmonium.399501f0.png"
+  }
+]);
