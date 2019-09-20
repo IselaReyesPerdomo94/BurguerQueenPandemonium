e022d3cca9f1616534f14ca06f119eebcf1d8aae
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-09-19 23:00:02 -0500

    Se modificó metodo para iniciar sesión utilizando correo y contraseña

--- my-app/src/components/password/index.jsx
+++ my-app/src/components/password/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import "./style.css";
 import TextField from '@material-ui/core/TextField';
 
-const PasswordInput = ({labelText}) => {
+const PasswordInput = ({labelText, value, onChange}) => {
     return (
-    <TextField id="outlined-password-input" label={labelText} type="password" autoComplete="current-password" margin="normal" variant="outlined" className="password-input"/>
+    <TextField id="outlined-password-input" label={labelText} type="password" autoComplete="current-password" margin="normal" variant="outlined" className="password-input" value={value} onChange={onChange}/>
     )
 }
 
