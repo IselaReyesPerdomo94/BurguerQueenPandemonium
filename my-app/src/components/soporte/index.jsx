85e75357cf850c626fa2a80a1d0aefacde752a5b
carolinaab <tau_karo@hotmail.com>
2019-09-26 17:11:13 -0500

    creando funcion cambio de contraseña

--- my-app/src/components/soporte/index.jsx
+++ my-app/src/components/soporte/index.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import EntryButton from './../Buttons/EntryButton/index.jsx';
 import './style.css';
 
-const Support = ({openModal}) => {
+const Support = ({ openModal }) => {
 
-    
     return (
         <div className="button-container">
-            <EntryButton className="button-support" onClick={openModal}text=" Cambiar clave de seguridad" />
+            <EntryButton className="button-support" onClick={openModal} text=" Cambiar clave de seguridad" />
         </div>
     )
-};
 
 
+}
 export default Support;
