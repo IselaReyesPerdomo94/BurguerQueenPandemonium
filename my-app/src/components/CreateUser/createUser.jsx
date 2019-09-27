612563da25f386be4d6c3eedc1fa8c21e3a5593e
rosario VS <paradisekiss-yaoi@hotmail.com>
2019-09-27 13:54:08 -0500

    Agregando primera vista de Inventario y Corte de caja

--- my-app/src/components/CreateUser/createUser.jsx
+++ my-app/src/components/CreateUser/createUser.jsx
@@ -4,20 +4,6 @@ import InputEmail from '../inputEmail/index';
 import InputMobile from '../inputMobile/index';
 import TextErrors from '../textErrors/index';
 
-const CreateUser = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [mobile, setMobile] = useState('');
-    const handleNameChange = (e) => {
-        setName(e.target.value)
-    }
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value)
-    }
-    const handleMobileChange = (e) => {
-        setMobile(e.target.value)
-    }
-    
 const CreateUser = ({handleEmailChange, handleMobileChange, handleNameChange, alert, error, success, name, email, mobile}) => {
     return (
         <Fragment>
@@ -30,6 +16,6 @@ const CreateUser = ({handleEmailChange, handleMobileChange, handleNameChange, al
         </Fragment>
     )
 }
-}
+
 
 export default CreateUser;
