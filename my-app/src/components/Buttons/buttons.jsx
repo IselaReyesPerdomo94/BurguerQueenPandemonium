32388fd4f9a674b2c91cb9b06965d11a1700b8bf
MonseCuevas <monsis.cuevas@gmail.com>
2019-09-29 11:57:13 -0500

    se agrega primera pantalla de comandas,completa

--- my-app/src/components/Buttons/buttons.jsx
+++ my-app/src/components/Buttons/buttons.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './style.css';
 
-const Button = ({text, client}) => {
+const Button = ({text, client, onClick}) => {
     return(
-        <button className="button comandas-box">
+        <button className="button comandas-box" onClick={onClick}>
             <span>{text}</span>
             <span>{client}</span>
         </button>
