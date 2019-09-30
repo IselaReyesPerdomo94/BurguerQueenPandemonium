8824ce03fdc58c43eb2787e7bb1e90aada243885
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-09-29 22:50:12 -0500

    Updates

---
+++ precache-manifest.3a613d8bdfe0b3f6c4d6fb218553116c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d385b9d07e4d2a93135b3ef049b4b5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8a846957244ec40a7fd4",
+    "url": "./static/css/2.c7ab29bb.chunk.css"
+  },
+  {
+    "revision": "99bd79d0d9f211a9f81d",
+    "url": "./static/css/main.78605753.chunk.css"
+  },
+  {
+    "revision": "8a846957244ec40a7fd4",
+    "url": "./static/js/2.f871838c.chunk.js"
+  },
+  {
+    "revision": "99bd79d0d9f211a9f81d",
+    "url": "./static/js/main.b68371a7.chunk.js"
+  },
+  {
+    "revision": "64691b40530ddb61605e",
+    "url": "./static/js/runtime~main.af7868da.js"
+  },
+  {
+    "revision": "399501f0dc1119d29363c264bb3444e0",
+    "url": "./static/media/pandmonium.399501f0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2db5fcec5b7b854cd114108be20d26db.js"
+  "./precache-manifest.3a613d8bdfe0b3f6c4d6fb218553116c.js"
 );
 
 self.addEventListener('message', (event) => {
