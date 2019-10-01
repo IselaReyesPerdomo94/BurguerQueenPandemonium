dfa5ec785dc19a144dae2b33837905fdd5c27ac8
carolinaab <tau_karo@hotmail.com>
2019-10-01 14:29:32 -0500

    se agrega funcion que limpia los inputs de modal agregar cuenta

--- my-app/src/views/comandas/comandas.jsx
+++ my-app/src/views/comandas/comandas.jsx
@@ -5,7 +5,7 @@ import Input from '../../components/CleanInput/index';
 import CleanModal from '../../components/cleanmodal/index';
 import EntryButton from '../../components/Buttons/EntryButton/index';
 import Dropdown from '../../components/Dropdown/index';
-import {db} from '../../firebase/index';
+import { db } from '../../firebase/index';
 import './style.css';
 
 const Comandas = (props) => {
@@ -20,7 +20,8 @@ const Comandas = (props) => {
         setVisible(visibility);
     }
 
-    const handlingTableModal= (state) => {
+    const handlingTableModal = (state) => {
+        cleanInputAddAccount()
         setOpenTableModal(state)
     }
 
@@ -28,17 +29,21 @@ const Comandas = (props) => {
     const handleNameTable = (e) => setNameTable(e.target.value)
 
     const addNewTable = () => {
-        const newTable = table.concat([{number: numberTable, name: nameTable}]);
+        const newTable = table.concat([{ number: numberTable, name: nameTable }]);
         setTable(newTable);
         handlingTableModal(false);
     }
 
+    const cleanInputAddAccount = () => {
+        setNumberTable('')
+        setNameTable('')
+    }
     const getUserCollectionForDropdown = () => {
         db.collection('users').get().then(querySnapshot => {
             let elements = [];
             querySnapshot.forEach(item => {
                 elements.push(item.data());
-            });           
+            });
             setUsers(elements);
         });
     }
@@ -50,75 +55,75 @@ const Comandas = (props) => {
     }, []);
 
 
-    const {menu}= props;
+    const { menu } = props;
     const tableContent = (
         <Fragment>
             <p>Número de cuenta</p>
-            <Input 
+            <Input
                 label={"Número de mesa"}
                 type={"number"}
                 value={numberTable}
                 onChange={handleNumberTable}
             />
-            <Input 
+            <Input
                 label={"Nombre"}
                 type={"text"}
                 value={nameTable}
                 onChange={handleNameTable}
-        />
+            />
         </Fragment>
     )
-        return (
-            <Fragment>
-                <CleanModal 
-                title="Agregar cuenta" 
+    return (
+        <Fragment>
+            <CleanModal
+                title="Agregar cuenta"
                 open={openTableModal}
-                close={()=>handlingTableModal(false)}
+                close={() => handlingTableModal(false)}
                 content={tableContent}
                 footer={
-                    <EntryButton 
-                    text="CONFIRMAR"
-                    onClick={addNewTable}
-                    className="confirm-table"
+                    <EntryButton
+                        text="CONFIRMAR"
+                        onClick={addNewTable}
+                        className="confirm-table"
                     />}
-                />
-                <div className="wrapper">
-                     {menu}
-                    <main className="main-comandas">
-                        <Tittle 
-                        color="#0288D1" 
-                        text="Comandas" 
-                        />
+            />
+            <div className="wrapper">
+                {menu}
+                <main className="main-comandas">
+                    <Tittle
+                        color="#0288D1"
+                        text="Comandas"
+                    />
                     <div className="options-select">
                         <Dropdown titulo="¿QUIEN ATIENDE?" options={
-                            users.map(item => 
+                            users.map(item =>
                                 <option key={item.telefono} value={item.nombre}>{item.nombre}</option>
                             )
                         } optionDefault="USUARIO" />
                         <Dropdown titulo="SUCURSAL" optionDefault="SUCURSAL" options={
                             sucursalOptions.map(opt => <option key={opt} value={opt}>{opt}</option>)
-                        }/>
+                        } />
                     </div>
                     <div className="table-box">
-                            {
-                                table.map(client => <Userandbranch 
-                                        numberTable={client.number}
-                                        nameTable={client.name}
-                                        changeVisibility={changeVisibility}
-                                        visible={visible}
-                                        key={client.name}
-                                        />
-                                    )
-                            }
-                       
+                        {
+                            table.map(client => <Userandbranch
+                                numberTable={client.number}
+                                nameTable={client.name}
+                                changeVisibility={changeVisibility}
+                                visible={visible}
+                                key={client.name}
+                            />
+                            )
+                        }
+
                     </div>
-                     <div className={`btn-add ${visible? "no-visible": "visible"}`}>
-                            <EntryButton text="AGREGAR" onClick={()=> handlingTableModal(true)}/>
+                    <div className={`btn-add ${visible ? "no-visible" : "visible"}`}>
+                        <EntryButton text="AGREGAR" onClick={() => handlingTableModal(true)} />
                     </div>
-                    </main>
-                </div>
-            </Fragment>
-        )
+                </main>
+            </div>
+        </Fragment>
+    )
 
 }
 
