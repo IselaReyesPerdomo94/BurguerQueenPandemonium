92907093d4c80c373f8d8d3618a511929ca863a1
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-10-02 20:54:41 -0500

    Se agregó componente nuevo

--- my-app/src/components/inputEmpty/index.jsx
+++ my-app/src/components/inputEmpty/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './style.css';
 import TextField from '@material-ui/core/TextField';
 
-const InputEmpty = ({text, value, onChange}) => {
+const InputEmpty = ({text, value, onChange, className}) => {
   return (
     <TextField
       id="outlined-name-input"
@@ -12,7 +12,7 @@ const InputEmpty = ({text, value, onChange}) => {
       autoComplete="mobile"
       margin="normal"
       variant="outlined"
-      className="mobile-input"
+      className={`mobile-input ${className}`}
       value={value}
       onChange={onChange}
     />
