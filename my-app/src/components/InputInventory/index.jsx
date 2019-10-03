dcb176eca55d63ba2e2e7e6b07fb08fd15df30c1
LedaHuerta <ledamhuerta@gmail.com>
2019-10-03 00:18:28 -0500

    modificando onchange de amountToday

--- my-app/src/components/InputInventory/index.jsx
+++ my-app/src/components/InputInventory/index.jsx
@@ -67,7 +67,7 @@ const InputInventory = (props) => {
             <InputEmpty 
                 text="Cantidad diaria" 
                 className="input-box-empty" 
-                handleChangeTodayAmount={handleChangeTodayAmount}
+                onChange={handleChangeTodayAmount}
                 />
             <Dropdown 
                 optionDefault="Medida" 
