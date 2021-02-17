89ffd42e85a05f55c583594cca6a4553029948a9
Isela Reyes Perdomo <48400000+IselaReyesPerdomo94@users.noreply.github.com>
2021-02-16 20:08:29 -0800

    Update index.js
    Deleting api keys

--- my-app/src/firebase/index.js
+++ my-app/src/firebase/index.js
@@ -3,13 +3,13 @@ import 'firebase/auth';
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyC7FSK03JcEV7HtO20cXB03ncK_afcfSc4",
+    apiKey: "",
     authDomain: "pandemonium-5a001.firebaseapp.com",
     databaseURL: "https://pandemonium-5a001.firebaseio.com",
     projectId: "pandemonium",
     storageBucket: "pandemonium.appspot.com",
     messagingSenderId: "472468721699",
-    appId: "1:472468721699:web:6c8dfa1b1e77d01f296196"
+    appId: ""
 }
 
 export const firebaseApp = firebase.initializeApp(config);
